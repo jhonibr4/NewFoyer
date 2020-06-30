052385a9c9c4aad7cdf329fbf9fe873dcc01f3d2
jhonibr4 <jhony.braz@hotmail.com>
2020-06-29 23:19:14 -0300

    Desenvolvimento do backend e frontend

--- foyer/App.js
+++ foyer/App.js
@@ -3,10 +3,9 @@ import Routes from './src/routes';
 import 'react-native-gesture-handler';
 
 
-export default class App extends Component{
-    async componentDidMount(){
 
-    }
+export default class App extends Component{
+ 
     render(){
         return(
             <Routes />
